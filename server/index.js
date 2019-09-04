c285790f69151d880e68651107eb29d8535f13ae
Oscar Reyes <reyes.oscarh@gmail.com>
2019-09-04 18:52:45 -0500

    mongodb config addes

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 'use strict';
 //Config data base
-
 var mongoose = require('mongoose');
 var app = require('./app');
 var port = 5000;
 
-app.listen(port, function(){
-    console.log("Servidor api rest escuchando en http://localhost:"+port);
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/musicsam',{ useNewUrlParser: true }, (err,res) =>{
+    if(err){
+        throw err;
+    }
+    else{
+        console.log("Conexion a base de datos exitosa");
+        app.listen(port, function(){
+            console.log("Servidor api rest escuchando en http://localhost:"+port);
+        })
+    }
 })
-
